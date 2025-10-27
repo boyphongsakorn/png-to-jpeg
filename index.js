a77b7de8bb6e1697782a0e5bb5248843f4d7be1d
Phongsakorn Wisetthon <3013842+boyphongsakorn@users.noreply.github.com>
2025-10-27 11:59:08 +0700

    require

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
-import Png from "png-js";
-import { encode } from "jpeg-js";
+// import Png from "png-js";
+// import { encode } from "jpeg-js";
 
-export default opts => {
+// export default opts => {
+//     opts = Object.assign({quality: 50}, opts);
+
+//     if (!Number.isInteger(opts.quality)) {
+//         return Promise.reject(new Error("'quality' only accepts integers"));
+//     }
+
+//     if (opts.quality < 1 || opts.quality > 100) {
+//         return Promise.reject(new Error("'quality' has to be between 1-100"));
+//     }
+
+//     return buf => {
+//         let png = new Png(buf);
+//         return new Promise(resolve => png.decode(resolve))
+//         .then(data => encode({data, width: png.width, height: png.height}, opts.quality).data);
+//     }
+// }
+
+// Change 'import' to 'require'
+const Png = require("png-js");
+const { encode } = require("jpeg-js");
+
+// Change 'export default' to 'module.exports'
+module.exports = opts => {
+    // Set default quality
     opts = Object.assign({quality: 50}, opts);
 
+    // Input validation for quality
     if (!Number.isInteger(opts.quality)) {
         return Promise.reject(new Error("'quality' only accepts integers"));
     }
@@ -12,9 +37,14 @@ export default opts => {
         return Promise.reject(new Error("'quality' has to be between 1-100"));
     }
 
+    // Return the function that processes the buffer
     return buf => {
         let png = new Png(buf);
+        // Decode the PNG buffer
         return new Promise(resolve => png.decode(resolve))
-        .then(data => encode({data, width: png.width, height: png.height}, opts.quality).data);
+        .then(data => 
+            // Encode to JPEG using jpeg-js
+            encode({data, width: png.width, height: png.height}, opts.quality).data
+        );
     }
 }
